4915a23746383486f4efa2f44cfe3f46b4db4824
dharsan05 <dharsanrajvir@gmail.com>
2019-03-20 23:42:46 +0530

    pwd

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -8,16 +8,12 @@ public class Git {
 		
 
 	}
+	
+	public void pwd() {
+		System.out.println("password");
 
-<<<<<<< HEAD
-	public void checkbox() {
-		System.out.println("Checkbox");
-=======
-	public void dropdown() {
-		System.out.println("Dropdown");
->>>>>>> 698699209c26fec482e6f847d22a4412b67eb38c
-
+	}
 	}
 	
 	
-}
+
