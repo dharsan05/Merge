7c7c265d66c077c47c8d11d732c712608a8cf55b
dharsan05 <dharsanrajvir@gmail.com>
2019-03-20 23:28:34 +0530

    added

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -1,5 +1,18 @@
 package org.git;
 
 public class Git {
+	
+	public void obj() {
+		
+		System.out.println("New object");
+		
 
+	}
+
+	public void checkbox() {
+		System.out.println("Checkbox");
+
+	}
+	
+	
 }
