b134515d07b5006ba4c1fc8355bc226c146767fe
dharsan05 <dharsanrajvir@gmail.com>
2019-03-20 23:24:38 +0530

    Updated

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -1,5 +1,18 @@
 package org.git;
 
 public class Git {
+	
+	public void obj() {
+		
+		System.out.println("New object");
+		
 
+	}
+
+	public void dropdown() {
+		System.out.println("Dropdown");
+
+	}
+	
+	
 }
